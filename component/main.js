7ec0fd92dd9362ca00e9a73cbc02e7ad8741ebda
Matt Thomas <matsinet@gmail.com>
2024-08-27 10:08:22 -0500

    Change filenames to lowercase part 1

--- component/main.js
+++ component/main.js
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import html from "html-literal";
-import * as views from "./views";
+import * as views from "../views";
 
 export default (state) => html`
   ${views[state.view](state)}
